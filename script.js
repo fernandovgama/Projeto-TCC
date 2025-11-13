7d98668b44f10651943ddba50da2b75ce943eef3
Fernando Gama <fernandogamav122@gmail.com>
2025-11-13 14:50:42 -0300

    adicionado outras seções a estrutura, modificação do header e primeiras seções

---
+++ script.js
@@ -0,0 +1,10 @@
+//Mudança de estilo do header ao rolar a página
+document.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
